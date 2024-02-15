a5bd96238053bb018863c18f153ba78be3fedf05
Jay Suthar <76590020+Jay-Suthar@users.noreply.github.com>
2024-02-15 19:48:12 +0530

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,3 +1,3 @@
 from pymongo import MongoClient
-MONGO_URI = "mongodb+srv://jay_prekshit_2001:grsuthar_shachi_javi_avi_2001@cluster0.ewpmu.mongodb.net"
+MONGO_URI = ""
 conn = MongoClient(MONGO_URI)
